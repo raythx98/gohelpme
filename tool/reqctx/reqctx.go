a49207fe1fb7f685a48c9ffeae408e2fa2f0c9af
Ray Toh <raythx98@gmail.com>
2024-06-24 12:37:02 +0800

    Add comments to reqctx

--- tool/reqctx/reqctx.go
+++ tool/reqctx/reqctx.go
@@ -4,13 +4,31 @@ import (
 	"context"
 )
 
+// Key is used when writing to context using context.WithValue(parent, Key, Value).
 var Key = "ReqCtx"
 
+// Value stores commonly used metadata to log consistently across microservices.
+//
+// Uninitialized fields will not be logged due to json tag `json:"omitempty"`.
 type Value struct {
-	RequestId string
-	UserId    *int64 `json:"Userid,omitempty"`
+	RequestId      string
+	UserId         *int64  `json:"Userid,omitempty"`
+	IdempotencyKey *string `json:"idempotencyKey,omitempty"`
 }
 
+// New initializes *Value with a required requestId.
+//
+// Value is mutable, and can be modified in-place using setter methods.
+// Example:
+//
+//	valuePtr := reqctx.New(uuid.NewString())
+//	valuePtr.SetUserId(1)
+//	fmt.Println(valuePtr.UserId) // 1
+func New(requestId string) *Value {
+	return &Value{RequestId: requestId}
+}
+
+// SetUserId modifies UserId of Value in place.
 func (v *Value) SetUserId(userId int64) {
 	if v == nil {
 		return
@@ -18,10 +36,21 @@ func (v *Value) SetUserId(userId int64) {
 	v.UserId = &userId
 }
 
-func New(requestId string) *Value {
-	return &Value{RequestId: requestId}
+// SetIdempotencyKey modifies IdempotencyKey of Value in place.
+func (v *Value) SetIdempotencyKey(idemKey string) {
+	if v == nil {
+		return
+	}
+	v.IdempotencyKey = &idemKey
 }
 
+// GetValue retrieves a pointer to Value.
+//
+// Value is mutable, a common usage is to retrieve from context to set values.
+// Example:
+//
+//	ctx := context.WithValue(context.Background(), reqctx.Key, reqctx.New(uuid.NewString()))
+//	reqctx.GetValue(ctx).SetUserId(1)
 func GetValue(ctx context.Context) *Value {
 	if reqCtx, ok := ctx.Value(Key).(*Value); ok {
 		return reqCtx
