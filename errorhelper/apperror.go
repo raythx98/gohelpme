0139a9d7e87bdd3a22f5a3df6e3f81910a384b2b
Ray Toh <raythx98@gmail.com>
2025-03-16 14:36:05 +0800

    Define custom error Is()

--- errorhelper/apperror.go
+++ errorhelper/apperror.go
@@ -1,6 +1,9 @@
 package errorhelper
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // AppError are analogous to caught/known exceptions
 type AppError struct {
@@ -20,4 +23,10 @@ func NewAppError(code int, message string, err error) *AppError {
 
 func (e *AppError) Error() string {
 	return fmt.Sprintf("Message: %s, Code: %d, Err: %v", e.Message, e.Code, e.err)
+}
+
+func (e *AppError) Is(target error) bool {
+	var targetAppError *AppError
+	ok := errors.As(target, &targetAppError)
+	return ok && targetAppError.Code == e.Code && targetAppError.Message == e.Message
 }
